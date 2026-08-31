ca3b5ddfe35260c8c80b17373036cd3cd53f65a8
Mahmoud_Mohammed <git config --global user.email>
2026-08-31 17:32:31 +0300

    initlize app and create news model

---
+++ lib/models/news_model.dart
@@ -0,0 +1,13 @@
+class NewsModel {
+  String title;
+  String subTitle;
+  String image;
+  NewsModel({required this.title, required this.subTitle, required this.image});
+  factory NewsModel.fromJson(json) {
+    return NewsModel(
+      title: json['title'] ?? '',
+      subTitle: json['description'] ?? '',
+      image: json['urlToImage'] ?? '',
+    );
+  }
+}
