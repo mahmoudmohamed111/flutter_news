064435973b706f7478702d0e8dd12391f5f949de
Mahmoud_Mohammed <git config --global user.email>
2026-09-03 14:54:03 +0300

    create Trending news view

---
+++ lib/views/trending_news_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news/services/trending_news_services.dart';
+import 'package:flutter_news/widgets/news_item.dart';
+
+// ignore: must_be_immutable
+class Trending_News_Page extends StatefulWidget {
+  Trending_News_Page({super.key});
+  String? title;
+  @override
+  State<Trending_News_Page> createState() => _Trending_News_PageState();
+}
+
+class _Trending_News_PageState extends State<Trending_News_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text("Trending News"),
+        centerTitle: true,
+        titleTextStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      body: FutureBuilder(
+        future: TrendingNewsServices().gettrending_news(),
+
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 10,
+                  ),
+                  child: News_item(newsModel: snapshot.data![index]),
+                );
+              },
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
